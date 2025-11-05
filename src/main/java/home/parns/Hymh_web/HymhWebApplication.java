ce55b19c9ad8eb49c520e4a41d2bae313805fd15
parns <bobchadoo@gmail.com>
2025-11-05 23:34:13 +0900

    init

---
+++ src/main/java/home/parns/Hymh_web/HymhWebApplication.java
@@ -0,0 +1,13 @@
+package home.parns.Hymh_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HymhWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HymhWebApplication.class, args);
+	}
+
+}
