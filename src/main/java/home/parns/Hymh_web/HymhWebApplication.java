df96eca333d8b5f0da86a0a91ea7c674c898a975
parns <bobchadoo@gmail.com>
2025-11-06 21:46:41 +0900

    DB없이 일단 돌리게 패치

--- src/main/java/home/parns/Hymh_web/HymhWebApplication.java
+++ src/main/java/home/parns/Hymh_web/HymhWebApplication.java
@@ -2,8 +2,11 @@ package home.parns.Hymh_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class
+})
 public class HymhWebApplication {
 
 	public static void main(String[] args) {
